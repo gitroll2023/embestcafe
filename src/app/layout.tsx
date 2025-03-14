1d296a6acb18ccfe8229856f5635df52ecd18964
Woojin <class2023.05@gmail.com>
2025-03-14 15:20:02 +0900

    메타오류

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,11 +6,11 @@ export const metadata: Metadata = {
   title: 'EM베스트 | 네이버 대표 FPS 장비 카페',
   description: '네이버 대표 FPS 장비 카페 EM베스트에서 제공하는 마케팅 서비스로 스마트스토어 판매를 강화하세요.',
   keywords: 'FPS 장비 입점, FPS 카페 마케팅, 게이머 타겟팅, 스마트스토어 마케팅, EM베스트',
-  metadataBase: new URL('http://embest-ads.kro.kr/'),
+  metadataBase: new URL('http://embest-ads.kro.kr'),
   openGraph: {
     title: 'EM베스트 | 네이버 대표 FPS 장비 카페',
     description: '네이버 대표 FPS 장비 카페 EM베스트에서 제공하는 마케팅 서비스로 스마트스토어 판매를 강화하세요.',
-    url: 'http://embest-ads.kro.kr/',
+    url: 'http://embest-ads.kro.kr',
     type: 'website',
   },
 };
